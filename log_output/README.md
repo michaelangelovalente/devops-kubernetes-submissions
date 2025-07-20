9067c7a3074ebd84b1ebaa312556684b782e8c4d
michaelangelovalente <miky10.mv@gmail.com>
2025-07-20 15:20:18 +0200

    Base golang backend

---
+++ log_output/README.md
@@ -0,0 +1,44 @@
+# Project log_output
+
+One Paragraph of project description goes here
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+See deployment for notes on how to deploy the project on a live system.
+
+## MakeFile
+
+```
+
+Build the application
+```bash
+make build
+```
+
+Run the application
+```bash
+make run
+```
+Create DB container
+```bash
+make docker-run
+```
+
+Shutdown DB Container
+```bash
+make docker-down
+```
+Live reload the application:
+```bash
+make watch
+```
+
+Clean up binary from the last build:
+```bash
+make clean
+```
+
+<!-- Run build make command with tests -->
+<!-- ```bash -->
+<!-- make all -->
---
+++ log_output/internal/server/server.go
@@ -0,0 +1,100 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log_output/internal/logger"
+	"log_output/internal/storage"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type Server struct {
+	*http.Server
+	port       int
+	logger     *logger.Logger
+	logStorage logger.LogStorage
+	loggerDone chan struct{}
+	wg         sync.WaitGroup
+}
+
+func NewServer() *Server {
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	if port == 0 {
+		port = 3000
+	}
+
+	// Initialize store mem
+	store := storage.NewMemoryStorage()
+	// store := logger.NewLogger()
+
+	interval := 5 * time.Second
+	// normally would get interval from env with godotenv...
+
+	config := logger.LoggerConfig{
+		Interval:      interval,
+		GenerateValue: logger.GenerateUUID, // parameterized func
+		TimeFormat:    time.RFC3339,        // ISO 8601 format with timezone
+	}
+
+	// Init Logger instnace
+	log := logger.NewLogger(config, store)
+	newServer := &Server{
+		Server: &http.Server{
+			Addr: fmt.Sprintf(":%d", port),
+			// Handler:      newServer.RegisterRoutes(),
+			IdleTimeout:  time.Minute,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 30 * time.Second,
+		},
+		port:       port,
+		logger:     log,
+		logStorage: store,
+		loggerDone: make(chan struct{}),
+	}
+
+	newServer.Handler = newServer.RegisterRoutes()
+
+	// Start bg logger
+	newServer.startLogger()
+
+	return newServer
+}
+
+// Override orig. httpServer.Shutdown() to clean up custom logger
+func (s *Server) Shutdown(ctx context.Context) error {
+	// Stop logger first
+	close(s.loggerDone)
+	s.wg.Wait()
+
+	// Original httpServer Shutdown call
+	return s.Server.Shutdown(ctx)
+}
+
+func (s *Server) startLogger() {
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+
+		// Create cancellable ctx
+		ctx, cancel := context.WithCancel(context.Background())
+
+		// sart diff goroutine used to wait for done signal
+		go func() {
+			<-s.loggerDone
+			cancel()
+		}()
+
+		// Start logger --> will continue running until context cancelled
+		if err := s.logger.StartLogger(ctx); err != nil {
+			// Logg err if start logger gives err  but continue running server
+			fmt.Printf("Logger error: %v")
+		}
+
+	}()
+}
