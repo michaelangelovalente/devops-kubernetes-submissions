cf2357e95f300c059f46ff30e49bb529c29044a8
michaelangelovalente <miky10.mv@gmail.com>
2025-08-05 17:16:11 +0200

    Base static site todo app for ex 1.05

--- Chapter-2/Part-3/e-1.05/todo_app/go.mod
+++ Chapter-2/Part-3/e-1.05/todo_app/go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/go-chi/cors v1.2.2
 	github.com/joho/godotenv v1.5.1
 )
+
+require github.com/a-h/templ v0.3.924 // indirect
--- Chapter-2/Part-3/e-1.05/todo_app/internal/server/routes.go
+++ Chapter-2/Part-3/e-1.05/todo_app/internal/server/routes.go
@@ -1,10 +1,10 @@
 package server
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
+	"todo_app/web"
 
+	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
@@ -22,19 +22,8 @@ func (s *Server) RegisterRoutes() http.Handler {
 		MaxAge:           300,
 	}))
 
-	r.Get("/", s.HelloWorldHandler)
-
+	// fileServer := http.FileServer(http.FS(web.Files))
+	// r.Handle("/views/*", fileServer)
+	r.Get("/", templ.Handler(web.Base()).ServeHTTP)
 	return r
-}
-
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
 }
