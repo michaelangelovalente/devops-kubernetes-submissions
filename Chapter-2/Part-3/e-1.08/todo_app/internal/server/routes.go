aec5243aaa0972b1c79d1413a1ba4f2de91ff7cb
michaelangelovalente <miky10.mv@gmail.com>
2025-08-26 07:51:42 +0200

    Ex 1.08: Copied project todo app from 1.06; Added ingress.yaml

---
+++ Chapter-2/Part-3/e-1.08/todo_app/internal/server/routes.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"net/http"
+	"todo_app/web"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	// fileServer := http.FileServer(http.FS(web.Files))
+	// r.Handle("/views/*", fileServer)
+	r.Get("/", templ.Handler(web.Base()).ServeHTTP)
+	return r
+}
